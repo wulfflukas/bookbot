87f6ebd9212705a07566d860cbbf272ba72f63fe
wulfflukas <wulfflukas@gmail.com>
2024-07-31 12:00:06 +0200

    read book

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-with open(path_to_file) as f:
-    for line in f:
-        print(line)
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_text(book_path)
+    print(text)
+    
+    
+def get_text(path):
+    with open(path) as f:
+        return f.read()
+        
+        
+if __name__ == "__main__": 
+    main()
